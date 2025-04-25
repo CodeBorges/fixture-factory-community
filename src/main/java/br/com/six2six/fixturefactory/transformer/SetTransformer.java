e17bc1a62493c50fe1739c8bd83233d0872711a4
joao.borges <joao.borges@accurate.com.br>
2025-04-25 20:18:17 -0300

    bugfix: update SetTransformer to fix mvn clean error

--- src/main/java/br/com/six2six/fixturefactory/transformer/SetTransformer.java
+++ src/main/java/br/com/six2six/fixturefactory/transformer/SetTransformer.java
@@ -44,9 +44,9 @@ public class SetTransformer implements Transformer {
     @SuppressWarnings({ "rawtypes", "unchecked" })
     private <T> T transformToConcreteTarget(Object value, Class<T> type) {
         if (EnumSet.class.isAssignableFrom(type)) {
-            return type.cast(EnumSet.copyOf((Collection) value));
+            return (T) type.cast(EnumSet.copyOf((Collection) value));
         } else {
-            return ReflectionUtils.newInstance(type, Arrays.asList((Collection) value));
+            return (T) ReflectionUtils.newInstance(type, Arrays.asList((Collection) value));
         }
     }
 }
